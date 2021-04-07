e144d83d7103e7cc1d47be7cb1a8a9a2964c54a8
enyolka <46387362+enyolka@users.noreply.github.com>
2021-04-07 14:06:43 +0200

    feat: project structure

---
+++ app/src/main/java/com/example/cosmeticszone/BelovedProductsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cosmeticszone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BelovedProductsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beloved_products)
+    }
+}
---
+++ app/src/main/java/com/example/cosmeticszone/ProductInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cosmeticszone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_info)
+    }
+}
